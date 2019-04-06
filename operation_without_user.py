b4700a7155d3d029b685fa89e1894a5ddc905057
Sahil Gupta <43669801+tonyhacker1@users.noreply.github.com>
2019-04-06 20:29:24 +0000

    Add files via upload

---
+++ operation_without_user.py
@@ -0,0 +1,34 @@
+import numpy
+x=numpy.array([[1,2,3],[1,2,3],[5,6,4]])
+y=numpy.array([[4,5,6],[7,8,9],[7,4,1]])
+
+#add()
+print("the element wise of addition of matrix is :")
+print(numpy.add(x,y))
+
+#subtract()
+print("the element wise of subtraction of matrix is :")
+print(numpy.subtract(x,y))
+
+#divide()
+print("the element wise of divided of matrix is :")
+print(numpy.divide(x,y))
+
+#multiply() :  This function is used to perform element wise matrix multiplication.
+print("the element wise of multiply of matrix is :") 
+print(numpy.multiply(x,y))
+
+#sqrt() : This function is used to compute the square root of each element of matrix.
+print ("The element wise square root is : ") 
+print (numpy.sqrt(x)) 
+
+#"T" : This argument used to transpose a specified matrix.
+print ("The transpose of given matrix is : ") 
+print (x.T) 
+
+#dot() : This function is used to compute the matrix multiplication, rather than element wise multiplication.
+print("the dot of matrix is :")
+print(numpy.dot(x,y))
+
+
+
